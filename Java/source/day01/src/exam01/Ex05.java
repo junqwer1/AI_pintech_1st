b2a5fe3940675c90f1c6afa46d25d00ba4de020a
junqwer1 <zzang12358@naver.com>
2024-10-07 16:53:47 +0900

    day01 수업 소스

---
+++ Java/source/day01/src/exam01/Ex05.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        int num1 = 65;
+        char num2 = 'A';
+        System.out.println(num1 == num2); // 자원이 같은 위치에 존재하는지 체크(주소 비교)
+    }
+}
---
+++ Java/source/day01/src/exam01/Ex07.java
@@ -0,0 +1,10 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+//        long num1 = 100000000000; // int -> long
+        long num1 = 100000000000l; // 처음부터 long으로 인식
+        long num3 = 10_000_000_000L;
+        byte num2 = 100; // int 100 -> byte 변환 (일 2번)
+    }
+}
